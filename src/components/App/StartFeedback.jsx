67228e3bb1debc75b6c7d92b2bae50a686eb6638
Abdulahi Kabare <abdullahikabare@gmail.com>
2024-02-05 00:57:13 -0600

    done

---
+++ src/components/App/StartFeedback.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const StartFeedback = () => {
+  return (
+    <Link to="/1">
+      <button>Start feedback</button>
+    </Link>
+  );
+};
+
+export default StartFeedback;
--- src/components/App/Understanding/Understanding.jsx
+++ src/components/App/Understanding/Understanding.jsx
@@ -1,34 +1,33 @@
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { ADD_UNDERSTANDING } from "../../../store";
 
 const Understanding = () => {
-  const [understandingPost, setUnderstandingPost] = useState({});
+  const [understandingPost, setUnderstandingPost] = useState(0);
 
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    dispatch({ type: "ADD_FEELING_REVIEW", payload: FeelingPost });
-    history.push("/understanding");
-
-    return (
-      <>
-        <form>
-          onSubmit={handleSubmit}
-          <h2>How Well Are You Understanding The Content?</h2>
-          <input
-            onChange={(event) =>
-              setUnderstandingPost({ Feeling: event.target.value })
-            }
-            type="number"
-          ></input>
-          <button>Next</button>
-        </form>
-      </>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD_UNDERSTANDING, payload: understandingPost });
+    history.push("/3");
   };
+  return (
+    <>
+      <h1>How are you understanding the content?</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="understandingPost"
+          value={understandingPost}
+          onChange={(e) => setUnderstandingPost(e.target.value)}
+          required
+        />
+        <button type="submit">Next</button>
+      </form>
+    </>
+  );
 };
 export default Understanding;
